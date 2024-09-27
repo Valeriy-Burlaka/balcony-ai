db001dfc860f43478218bb60656dd9faa8654d18
Valeriy Burlaka <valeriy.burlaka88@gmail.com>
2024-09-27 20:31:47 +0200

    clean up image_processing module a bit

--- src/birds/lib/image_preprocessing.py
+++ src/birds/lib/image_preprocessing.py
@@ -6,10 +6,6 @@ import tensorflow as tf
 
 print(f"Process PID: {os.getpid()}")
 
-img_path = "test-detection/clips-split-by-frames/frame00075.png"
-model_path = "/Users/val/.cache/kagglehub/models/tensorflow/efficientdet/tensorFlow2/d1/1"
-
-# std_height, std_width = 640, 640
 target_size = 640
 gray_fill = (128, 128, 128)
 
@@ -41,38 +37,33 @@ def downsample_image(image: np.ndarray, target_size: int) -> np.ndarray:
 
     return cv2.resize(image, (target_size, target_size), cv2.INTER_LANCZOS4)
 
-def normalize(image: np.ndarray) -> np.ndarray:
-    return image / 255.0
+def read_image(image_path: str) -> np.ndarray:
+    return cv2.imread(image_path)
 
-def preprocess_image(img_path: str):
-    img = cv2.imread(img_path)
+def preprocess_image(image: np.ndarray) -> np.ndarray:
     # Convert the image from BGR to RGB as required by the TFLite model.
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    img = letterbox_landscape_image_to_square(img)
+    image = letterbox_landscape_image_to_square(image)
 
-    img = downsample_image(img, target_size=target_size)
+    image = downsample_image(image, target_size=target_size)
 
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-    return img
+    return image
+
+def normalize_for_tf(image: np.ndarray) -> np.ndarray:
+    # image = image / 255.0  # not required by efficientdet d2, although I don't see a Rescaling layer in it either :shrug:
+
+    # The operation tf.expand_dims(sample_image_t, axis=0) in TF adds a new dimension to the tensor T at the specified (0) axis.
+    # If the `normalized` image originally had a shape of (640, 640, 3), after applying tf.expand_dims(sample_image_t, axis=0),
+    # its new shape becomes (1, 640, 640, 3), effectively turning it into a "batch" of one image.
+    #  (Read more details here: https://claude.ai/chat/3805017f-9196-4989-9c66-0d3309a4f4df)
+    image = tf.expand_dims(image, axis=0)
+
+    return image
 
 def show_image(image: np.ndarray):
     cv2.imshow("Image", image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
-
-img = preprocess_image(img_path=img_path)
-cv2.imwrite("./preprocessed.png", img)
-
-normalized = normalize(img)
-# The operation tf.expand_dims(sample_image_t, axis=0) in TF adds a new dimension to the tensor T at the specified (0) axis.
-# If the `normalized` image originally had a shape of (640, 640, 3), after applying tf.expand_dims(sample_image_t, axis=0),
-# its new shape becomes (1, 640, 640, 3), effectively turning it into a "batch" of one image.
-#  (Read more details here: https://claude.ai/chat/3805017f-9196-4989-9c66-0d3309a4f4df)
-normalized = tf.expand_dims(normalized, axis=0)
-
-print("\nTensor:\n", type(normalized), normalized.dtype, normalized.shape)
-
-# model = tf.saved_model.load(model_path)
