0ccdaf217cbfe615d8861b84c633bf3e04eec906
Valeriy Burlaka <valeriy.burlaka88@gmail.com>
2024-10-13 21:28:02 +0200

    update test for the CLI

--- test-cli.sh
+++ test-cli.sh
@@ -2,7 +2,19 @@
 
 export PYTHONPATH="$(pwd)/src:$PYTHONPATH"
 
-./src/birds/main.py -vv extract-clip -i test-detection/video-clips/test-cli.mp4 -o output.mp4  --start 1 --end 5
-./src/birds/main.py -vv extract-frames -o test-cli/4-sec-video-to-frames -i output.mp4
-./src/birds/main.py -vv detect-objects -i test-cli/4-sec-video-to-frames/frame00005.png -o test-cli/object-detection/frame00005_processed_flower-pot.png --candidates "garden pot,flower pot"
-open test-cli/object-detection/frame00005_processed_flower-pot.png
+
+function tear_down() {
+    rm test-cli/output.mp4
+    rm -rf test-cli/4-sec-video-to-frames
+    rm -rf test-cli/object-detection
+}
+
+tear_down
+
+./src/birds/main.py -vv extract-clip -i test-cli/test-cli.mp4 -o test-cli/output.mp4  --start 1 --end 2
+./src/birds/main.py -vv extract-frames -i test-cli/output.mp4 -o test-cli/4-sec-video-to-frames
+./src/birds/main.py -vv detect-objects \
+    -i test-cli/4-sec-video-to-frames/output__frame00015.jpg \
+    -o test-cli/object-detection/frame00015_processed_flower-pot.jpg \
+    --candidates "garden pot,flower pot"
+open test-cli/object-detection/frame00015_processed_flower-pot.jpg
