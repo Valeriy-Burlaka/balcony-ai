4f36b9ed8fec6ee4aa683fc74893a7b396608aa2
Valeriy Burlaka <valeriy.burlaka88@gmail.com>
2024-11-25 22:24:30 +0100

    chore: increase test verbosity

--- test-cli.sh
+++ test-cli.sh
@@ -24,7 +24,7 @@ clean_outputs
 
 ./src/birds/main.py -vv extract-clip -i test-cli/test-cli.mp4 -o test-cli/output.mp4  --start 1 --end 2
 ./src/birds/main.py -vv extract-frames -i test-cli/output.mp4 -o test-cli/4-sec-video-to-frames
-./src/birds/main.py -vv detect-objects \
+./src/birds/main.py -vvv detect-objects \
     -i test-cli/test-cli.jpg \
     -o test-cli/object-detection/prediction.jpg
 
