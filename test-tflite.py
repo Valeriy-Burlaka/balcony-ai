af590669dbc303bc864d3d6961e5218cc14ba0e7
Valeriy Burlaka <valeriy.burlaka88@gmail.com>
2024-11-29 19:46:21 +0100

    wip: fix model filename; add main() wrapper

--- test-tflite.py
+++ test-tflite.py
@@ -7,20 +7,37 @@ from tflite_support.task import vision
 
 # https://github.com/tensorflow/examples/blob/master/lite/examples/object_detection/raspberry_pi/detect.py
 
-# Initialize the object detection model
-model = "efficientdet_lite0"
-enable_edgetpu = True
-num_threads = 4
+def main():
+    # Initialize the object detection model
+    model = "efficientdet_lite0.tflite"
+    enable_edgetpu = True
+    num_threads = 4
 
-base_options = core.BaseOptions(
-    file_name=model,
-    use_coral=enable_edgetpu,
-    num_threads=num_threads,
-)
-detection_options = processor.DetectionOptions(max_results=3, score_threshold=0.3)
-options = vision.ObjectDetectorOptions(
-    base_options=base_options,
-    detection_options=detection_options,
-)
+    base_options = core.BaseOptions(
+        file_name=model,
+        use_coral=enable_edgetpu,
+        num_threads=num_threads,
+    )
+    detection_options = processor.DetectionOptions(max_results=3, score_threshold=0.3)
+    options = vision.ObjectDetectorOptions(
+        base_options=base_options,
+        detection_options=detection_options,
+    )
 
-detector = vision.ObjectDetector.create_from_options(options)
+    detector = vision.ObjectDetector.create_from_options(options)
+    print(detector)
+
+
+if __name__ == "__main__":
+    main()
+    # # Load an image
+    # image_path = "test.jpg"
+    # image = cv2.imread(image_path)
+
+    # # Run inference
+    # results = detector.process(image)
+
+    # # Print the detection results
+    # for result in results:
+    #     print(result)
+    #     print("\n")
