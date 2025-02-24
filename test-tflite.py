378544e71695fb09d00e8871e861b789b2436233
Valeriy Burlaka <valeriy.burlaka88@gmail.com>
2025-02-24 23:54:04 +0100

    chore: save full file content before deletion

--- test-tflite.py
+++ test-tflite.py
@@ -19,13 +19,26 @@ def main():
         num_threads=num_threads,
     )
     detection_options = processor.DetectionOptions(max_results=3, score_threshold=0.3)
-    options = vision.ObjectDetectorOptions(
-        base_options=base_options,
-        detection_options=detection_options,
-    )
+    options = vision.ObjectDetectorOptions(base_options=base_options, detection_options=detection_options)
 
     detector = vision.ObjectDetector.create_from_options(options)
     print(detector)
+
+    # Classification
+    # # Imports
+    # from tflite_support.task import vision
+    # from tflite_support.task import core
+
+    # # Initialize options and turn on Coral Edge TPU delegation.
+    # base_options = core.BaseOptions(file_name=model_path, use_coral=True)
+    # options = vision.ImageClassifierOptions(base_options=base_options)
+
+    # # Create ImageClassifier from options.
+    # classifier = vision.ImageClassifier.create_from_options(options)
+
+    # # Run inference on Coral Edge TPU.
+    # image = vision.TensorImage.create_from_file(image_path)
+    # classification_result = classifier.classify(image)
 
 
 if __name__ == "__main__":
